50ecc980fb9d45e9c1c640120c323f676449320a
HeeSeok Kwon <heeseokkwon@HeeSeokui-MacBookAir.local>
2024-06-09 20:48:36 +0900

    docs: 로그인 화면 오류 수정

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -36,132 +36,134 @@ class LoginScreenState extends ConsumerState<LoginScreen> {
           return false;
         },
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/icons/app_logo.svg",
-                  width: 140,
-                  height: 177,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                OutlinedInput(
-                  controller: emailInputController,
-                  onChanged: (String email) {
-                    ref.read(loginButtonProvider.notifier).activate(
-                      emailInputController.text, passwordInputController.text);
-                  },
-                  hintText: '이메일을 입력하세요',
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 12),
-                OutlinedInput(
-                  controller: passwordInputController,
-                  onChanged: (String password) {
-                    ref.read(loginButtonProvider.notifier).activate(
-                      emailInputController.text, passwordInputController.text);
-                  },
-                  hintText: '비밀번호를 입력하세요',
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 22,
-                ),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        // radio button toggle 수행
-                        ref.read(autoLoginRadioProvider.notifier).toggle();
-                      },
-                      child: autoLoginRadioState ? SvgPicture.asset(
-                        "assets/icons/check/radio_checked.svg",
-                        width: 24,
-                        height: 24,
-                      ) : SvgPicture.asset(
-                        "assets/icons/check/radio_unchecked.svg",
-                        width: 24,
-                        height: 24,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/icons/app_logo.svg",
+                    width: 140,
+                    height: 177,
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  OutlinedInput(
+                    controller: emailInputController,
+                    onChanged: (String email) {
+                      ref.read(loginButtonProvider.notifier).activate(
+                        emailInputController.text, passwordInputController.text);
+                    },
+                    hintText: '이메일을 입력하세요',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 12),
+                  OutlinedInput(
+                    controller: passwordInputController,
+                    onChanged: (String password) {
+                      ref.read(loginButtonProvider.notifier).activate(
+                        emailInputController.text, passwordInputController.text);
+                    },
+                    hintText: '비밀번호를 입력하세요',
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 22,
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // radio button toggle 수행
+                          ref.read(autoLoginRadioProvider.notifier).toggle();
+                        },
+                        child: autoLoginRadioState ? SvgPicture.asset(
+                          "assets/icons/check/radio_checked.svg",
+                          width: 24,
+                          height: 24,
+                        ) : SvgPicture.asset(
+                          "assets/icons/check/radio_unchecked.svg",
+                          width: 24,
+                          height: 24,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      width: 6,
-                    ),
-                    const Text('자동로그인', style: CustomText.body6),
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                BlueTextButton(
-                  onPressed: () async {
-                    // TODO : 로그인 로직 추가
-                    if(emailInputController.text.isEmpty) {
-                      showAlertDialog(context: context, middleText: "이메일을 입력해주세요.");
-                      return;
-                    }
-                    if(passwordInputController.text.isEmpty) {
-                      showAlertDialog(context: context, middleText: "비밀번호를 입력해주세요.");
-                      return;
-                    }
-
-                    // TODO : 로그인 API 연동
-                    /*final result = await login(emailInputController.text, passwordInputController.text);
-                    
-                    if(context.mounted) {
-                      if(result) {
-                        context.goNamed('home_screen');
-                      } else {
-                        showAlertDialog(context: context, middleText: "이메일/비밀번호를 다시 확인해주세요.");
-                      } 
-                    }*/
-
-                    context.goNamed('home_screen');
-                  },
-                  text: '로그인',
-                  disabled: !loginButtonState,
-                ),
-                const SizedBox(
-                  height: 22,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        // TODO : 회원가입 화면 이동
-                        context.goNamed('register_first_screen');
-                      },
-                      child: Text(
-                        "회원가입",
-                        style: TextStyle(
-                            fontSize: CustomText.body7.fontSize,
-                            fontWeight: CustomText.body7.fontWeight,
-                            fontFamily: CustomText.body7.fontFamily,
-                            color: CustomColor.extraDarkGray),
+                      const SizedBox(
+                        width: 6,
                       ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        // TODO : 비밀번호 찾기 화면 이동
-                      },
-                      child: Text(
-                        "비밀번호 찾기",
-                        style: TextStyle(
-                            fontSize: CustomText.body7.fontSize,
-                            fontWeight: CustomText.body7.fontWeight,
-                            fontFamily: CustomText.body7.fontFamily,
-                            color: CustomColor.extraDarkGray),
+                      const Text('자동로그인', style: CustomText.body6),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  BlueTextButton(
+                    onPressed: () async {
+                      // TODO : 로그인 로직 추가
+                      if(emailInputController.text.isEmpty) {
+                        showAlertDialog(context: context, middleText: "이메일을 입력해주세요.");
+                        return;
+                      }
+                      if(passwordInputController.text.isEmpty) {
+                        showAlertDialog(context: context, middleText: "비밀번호를 입력해주세요.");
+                        return;
+                      }
+          
+                      // TODO : 로그인 API 연동
+                      /*final result = await login(emailInputController.text, passwordInputController.text);
+                      
+                      if(context.mounted) {
+                        if(result) {
+                          context.goNamed('home_screen');
+                        } else {
+                          showAlertDialog(context: context, middleText: "이메일/비밀번호를 다시 확인해주세요.");
+                        } 
+                      }*/
+          
+                      context.goNamed('home_screen');
+                    },
+                    text: '로그인',
+                    disabled: !loginButtonState,
+                  ),
+                  const SizedBox(
+                    height: 22,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // TODO : 회원가입 화면 이동
+                          context.goNamed('register_first_screen');
+                        },
+                        child: Text(
+                          "회원가입",
+                          style: TextStyle(
+                              fontSize: CustomText.body7.fontSize,
+                              fontWeight: CustomText.body7.fontWeight,
+                              fontFamily: CustomText.body7.fontFamily,
+                              color: CustomColor.extraDarkGray),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      InkWell(
+                        onTap: () {
+                          // TODO : 비밀번호 찾기 화면 이동
+                        },
+                        child: Text(
+                          "비밀번호 찾기",
+                          style: TextStyle(
+                              fontSize: CustomText.body7.fontSize,
+                              fontWeight: CustomText.body7.fontWeight,
+                              fontFamily: CustomText.body7.fontFamily,
+                              color: CustomColor.extraDarkGray),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
